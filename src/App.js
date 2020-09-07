a15bf9099eb544dde1a7f6b1b90afaf6c16489f8
apurva-modi <amodi002@odu.edu>
2020-09-07 01:09:42 -0700

    added all the helper methods

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import Row from "./Row";
+import "./App.css";
+import requests from "./request";
+import Nav from "./Nav";
+import Banner from "./Banner";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {Nav} */}
+      {/* {Banner} */}
+      <Nav />
+      <Banner />
+      <Row
+        title="Netfilx Originals"
+        fetchUrl={requests.fetchNetflixOrignals}
+        isLargeRow={true}
+      />
+      <Row
+        title="Trending Now"
+        fetchUrl={requests.fetchTrending}
+        isLargeRow={true}
+      />
+      <Row
+        title="Top Rated"
+        fetchUrl={requests.fetchTopRated}
+        isLargeRow={true}
+      />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Moives" fetchUrl={requests.fetchRomanceMovies} />
     </div>
   );
 }
